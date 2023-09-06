0532146dac457563c6aa195526aee53642f881a1
GustavoSilvalgs <luisgust06@gmail.com>
2023-09-06 16:33:52 -0300

    Creating a Service Discovery/Registry Application with Eureka

---
+++ 06-book-service/src/main/java/br/com/erudio/controller/BookController.java
@@ -0,0 +1,47 @@
+package br.com.erudio.controller;
+
+import br.com.erudio.model.Book;
+import br.com.erudio.proxy.CambioProxy;
+import br.com.erudio.repository.BookRepository;
+import br.com.erudio.response.Cambio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Date;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("book-service")
+public class BookController {
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private BookRepository repository;
+
+    @Autowired
+    private CambioProxy proxy;
+
+    @GetMapping(value = "/{id}/{currency}")
+    public Book findBook(
+        @PathVariable("id") Long id,
+        @PathVariable("currency") String currency
+    ) {
+
+        var book = repository.getReferenceById(id);
+        if (book == null) throw new RuntimeException("Book not Found");
+
+        var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
+
+        var port = environment.getProperty("local.server.port");
+        book.setEnvironment(port + " FEIGN");
+        book.setPrice(cambio.getConvertedValue());
+        return book;
+    }
+}
