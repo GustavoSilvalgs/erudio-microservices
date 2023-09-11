dc16d9c423d404d20f3ce9584fc908dca3ebb988
GustavoSilvalgs <luisgust06@gmail.com>
2023-09-11 11:40:51 -0300

    Setting up Swagger OpenAPI in the Book Microservice.

--- 06-book-service/src/main/java/br/com/erudio/controller/FooBarController.java
+++ 06-book-service/src/main/java/br/com/erudio/controller/FooBarController.java
@@ -1,13 +1,15 @@
 package br.com.erudio.controller;
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Foo bar")
 @RestController
 @RequestMapping("book-service")
 public class FooBarController {
@@ -15,6 +17,7 @@ public class FooBarController {
     private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
     @GetMapping("/foo-bar")
+    @Operation(summary = "Foo bar")
     //@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
     //@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
     //@RateLimiter(name = "default")
